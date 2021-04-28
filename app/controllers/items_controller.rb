db2be63b5e199d897ce7b7290b360125f460b7d5
vi4me <vitaliy.cherednichenko113@gmail.com>
2021-04-28 22:40:56 +0300

    16 another action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   layout false
   skip_before_action :verify_authenticity_token
-  before_action :find_item, only: %i[show edit update destroy]
+  before_action :find_item, only: %i[show edit update destroy upvote]
   before_action :admin?, only: %i[new create edit update destroy]
   after_action :show_info, only: %i[index]
 
@@ -22,6 +22,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def new
+
+  end
+
   def edit
     render body: 'Page not found', status: 404 unless @item
   end
@@ -35,12 +39,21 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-
     if @item.destroy.destroyed?
       redirect_to items_path
     else
       render json: item.errors, status: :unprocessable_entity
     end
+  end
+
+  def upvote
+    @item.increment! :votes_count
+    redirect_to items_path
+  end
+
+  def expensive
+    @items = Item.where('price > 50')
+    render :index
   end
 
   private
@@ -50,7 +63,8 @@ class ItemsController < ApplicationController
   end
 
   def admin?
-    render json: 'Acces denied', status: :forbidden unless params[:admin]
+    true
+    # render json: 'Acces denied', status: :forbidden unless params[:admin]
   end
 
   def show_info
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :items#, only: %i[index create]
+  resources :items do #, only: %i[index create]
+    get :upvote, on: :member
+    get :expensive, on: :collection
+  end
 end
