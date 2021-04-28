dd3e5409cd57406762533c56dfa60e21194f2a0e
vi4me <vitaliy.cherednichenko113@gmail.com>
2021-04-28 10:11:50 +0300

    9 params, save items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,26 @@
 class ItemsController < ApplicationController
+  layout false
+  skip_before_action :verify_authenticity_token
 
   def index
     @items = Item.all
     # render text: @items.map do |i|
     #   "#{i.name}: #{i.price}"
+    end
+
+    def create
+      item = Item.create(items_params)
+      if item.persisted?
+        render json: item, status: :created
+      else
+        render json: item.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def items_params
+      params.permit(:name, :price, :weight, :description, :real)
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
 
-  get '/items', to: 'items#index'
+  resources :items#, only: %i[index create]
 end
